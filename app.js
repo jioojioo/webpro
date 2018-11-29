e61e6bdfb7be0055c292879a21c564dc5c78f4a3
jioojioo <aaaqqqaqaq12@hanmail.net>
2018-11-29 12:56:49 +0900

    'mlab'

--- app.js
+++ app.js
@@ -33,9 +33,16 @@ module.exports = (app, io) => {
   // mongodb connect
   //=======================================================
   mongoose.Promise = global.Promise; // ES6 Native Promise를 mongoose에서 사용한다.
-  const connStr = 'mongodb://localhost/mjdb4';
+  // const connStr = 'mongodb://localhost/mjdb4';
   // 아래는 mLab을 사용하는 경우의 예: 본인의 접속 String으로 바꾸세요.
-  // const connStr = 'mongodb://dbuser1:mju12345@ds113825.mlab.com:13825/sampledb1';
+
+  //  const connStr ='mongodb://dbuser1:123456a@ds127994.mlab.com:27994/mjdb';
+
+  
+  const connStr = (process.env.NODE_ENV == 'production')?
+   'mongodb://dbuser1:123456a@ds127994.mlab.com:27994/mjdb':
+   'mongodb://localhost/mjdb4';
+
   mongoose.connect(connStr, {useMongoClient: true });
   mongoose.connection.on('error', console.error);
   //mongo ppt 
